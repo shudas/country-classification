f2e2a8cd3e2bae99b59178d33bf7e0aaac9a2449
Shu <shudas@umich.edu>
2015-04-19 16:33:13 -0400

    run country if its main only

--- countryclassifier.py
+++ countryclassifier.py
@@ -114,7 +114,7 @@ def trainNaiveBayes(countries):
 
             with open(trainingFolder + country + "/" + myFile, 'r') as f:
                 data = f.read()
-            
+
                 # Preprocess text
                 data = tokenizeText(data)
                 if removeStop:
@@ -155,7 +155,7 @@ def trainNaiveBayes(countries):
                     gram = " ".join([word, word2])
                 else:
                     continue
-            
+
             if ngram == 3:
                 if x+2 < len(text[c]):
                     word2 = text[c][x+1]
@@ -196,7 +196,7 @@ def testNaiveBayes(filename, classProbs, wordProbs, vocabSize, n, country):
             data = removeEmoticons(data)
         if stem:
             data = stemWords(data)
-    
+
     data.append("")
     temp = ""
     data = [temp] + data
@@ -233,7 +233,7 @@ def testNaiveBayes(filename, classProbs, wordProbs, vocabSize, n, country):
                 p += math.log10(wordProbs[c][gram])
             else:
                 p += math.log10(1.0/(n[c] + vocabSize))
-        probs[c] = math.log10(classProbs[c]) + p    
+        probs[c] = math.log10(classProbs[c]) + p
 
     v = list(probs.values())
     k = list(probs.keys())
@@ -243,27 +243,28 @@ def testNaiveBayes(filename, classProbs, wordProbs, vocabSize, n, country):
 #----------------------------------------------------------------------
 
 # Main
-countryFolders = [f for f in listdir(trainingFolder)]
-classProbs, wordProbs, vocabSize, n = trainNaiveBayes(countryFolders)
+if __name__ == "__main__":
+    countryFolders = [f for f in listdir(trainingFolder)]
+    classProbs, wordProbs, vocabSize, n = trainNaiveBayes(countryFolders)
 
-testFolders = [f for f in listdir(folder)]
-total = 0.0
-correct = 0.0
+    testFolders = [f for f in listdir(folder)]
+    total = 0.0
+    correct = 0.0
 
-# Predict for each test file
-for country in testFolders:
-    testFiles = [f for f in listdir(join(folder,country)) if isfile(join(folder,country,f))]
-    for testFile in testFiles:
-        prediction = testNaiveBayes(testFile, classProbs, wordProbs, vocabSize, n, country)
-        total += 1.0
-        if prediction == country:
-            correct += 1.0
+    # Predict for each test file
+    for country in testFolders:
+        testFiles = [f for f in listdir(join(folder,country)) if isfile(join(folder,country,f))]
+        for testFile in testFiles:
+            prediction = testNaiveBayes(testFile, classProbs, wordProbs, vocabSize, n, country)
+            total += 1.0
+            if prediction == country:
+                correct += 1.0
 
-print "Accuracy:", correct/total
+    print "Accuracy:", correct/total
 
-# Print top words for each country
-for c in wordProbs:
-    print c
-    sorted_x = sorted(wordProbs[c].items(), key=operator.itemgetter(1), reverse=True)
-    for x in range(0,50):
-        print sorted_x[x]         
+    # Print top words for each country
+    for c in wordProbs:
+        print c
+        sorted_x = sorted(wordProbs[c].items(), key=operator.itemgetter(1), reverse=True)
+        for x in range(0,50):
+            print sorted_x[x]
